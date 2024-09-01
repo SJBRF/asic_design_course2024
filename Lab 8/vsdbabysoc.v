07b8a4ca31590a4d491cd7f2f458417ca4b53958
Priyanka Agarwal <SJBRF@users.noreply.github.com>
2024-09-01 17:23:21 +0530

    Add files via upload

---
+++ Lab 8/vsdbabysoc.v
@@ -0,0 +1,39 @@
+module vsdbabysoc (
+   output wire OUT,
+   //
+   input  wire reset,
+   //
+   input  wire VCO_IN,
+   input  wire ENb_CP,
+   input  wire ENb_VCO,
+   input  wire REF,
+   //
+   // input  wire VREFL,
+   input  wire VREFH
+);
+
+   wire clk_pri;
+   wire [9:0] RV_TO_DAC;
+
+   rvmyth core_pri(
+      .OUT(RV_TO_DAC),
+      .clk_pri(clk_pri),
+      .reset(reset)
+   );
+
+   avsdpll pll (
+      .CLK(clk_pri),
+      .VCO_IN(VCO_IN),
+      .ENb_CP(ENb_CP),
+      .ENb_VCO(ENb_VCO),
+      .REF(REF)
+   );
+
+   avsddac dac (
+      .OUT(OUT),
+      .D(RV_TO_DAC),
+      // .VREFL(VREFL),
+      .VREFH(VREFH)
+   );
+   
+endmodule
